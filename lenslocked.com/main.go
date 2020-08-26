01b22381458dc3c405bc6bb1d4300db2c3d5d3c1
sxmrxzxr <tue95606@temple.edu>
2020-08-25 22:23:53 -0400

    initial commit

---
+++ lenslocked.com/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+
+	"./views"
+
+	"github.com/gorilla/mux"
+)
+
+var homeView *views.View
+var contactView *views.View
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(homeView.Render(w, nil))
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(contactView.Render(w, nil))
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	homeView = views.NewView("bootstrap", "views/home.gohtml")
+	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":3000", r)
+}
